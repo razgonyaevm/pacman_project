981b7d53eba1736e5ba6b8cd8378d8bdc04f25fb
razgonyaevm <razgonyaevm@gmail.com>
2022-02-13 16:05:46 +0500

    Отследили столкновения главного героя с блоками

--- scripts/pacman_class.py
+++ scripts/pacman_class.py
@@ -6,6 +6,9 @@ with open('data/map_size.txt') as map_size:
     m = list(map(int, map_size.readline().split()))
     size = width, height = m[0], m[1]
 
+with open('maps/first_map.txt') as map_file:
+    map_f = list(map(lambda x: x.strip(), map_file.readlines()))
+
 
 class Pacman(pygame.sprite.Sprite):
     pygame.init()
@@ -24,7 +27,8 @@ class Pacman(pygame.sprite.Sprite):
         if event.type == pygame.KEYDOWN:
             if pygame.key.get_pressed()[pygame.K_UP]:
                 if self.rect.y - 53 > -50:
-                    self.rect.y -= 53
+                    if map_f[(self.rect.y - 53) // 53][self.rect.x // 50] != '#':
+                        self.rect.y -= 53
                 else:
                     self.rect.y = height - 51
                 if self.key == 1:
@@ -36,7 +40,8 @@ class Pacman(pygame.sprite.Sprite):
                 self.key = 2
             elif pygame.key.get_pressed()[pygame.K_DOWN]:
                 if self.rect.y + 53 < height:
-                    self.rect.y += 53
+                    if map_f[(self.rect.y + 53) // 53][self.rect.x // 50] != "#":
+                        self.rect.y += 53
                 else:
                     self.rect.y = 0
                 if self.key == 1:
@@ -48,7 +53,8 @@ class Pacman(pygame.sprite.Sprite):
                 self.key = 4
             elif pygame.key.get_pressed()[pygame.K_LEFT]:
                 if self.rect.x - 50 > -48:
-                    self.rect.x -= 50
+                    if map_f[self.rect.y // 53][(self.rect.x - 50) // 50] != '#':
+                        self.rect.x -= 50
                 else:
                     self.rect.x = width - 50
                 if self.key == 1:
@@ -60,7 +66,8 @@ class Pacman(pygame.sprite.Sprite):
                 self.key = 3
             elif pygame.key.get_pressed()[pygame.K_RIGHT]:
                 if self.rect.x + 50 < width:
-                    self.rect.x += 50
+                    if map_f[self.rect.y // 53][(self.rect.x + 50) // 50] != '#':
+                        self.rect.x += 50
                 else:
                     self.rect.x = 0
                 if self.key == 2:
