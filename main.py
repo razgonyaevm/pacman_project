d604bcce9d0fc6dc4fb35de21dd7314819c2122c
razgonyaevm <razgonyaevm@gmail.com>
2022-02-13 14:41:44 +0500

    Добавлено ограничение на количество препятствий в уровне

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import os
 import random
 
 
-def generate_map(name, width, height):
+def generate_map(name, width, height, coin=100000):
+    """Функция для генерирования карт уровней. name - название файля для сохранения,
+    файл автоматически сохраняется в апку maps;
+    width - длина каждой строчки
+    height - количество строк в файле (не 0)
+    coin - необязательный параметр, нужен для упрощения уровня (ограничивает количество блоков в каждой строке)"""
+
     line = ''
+    correct = 0
     ans = []
     for i in range(height):
         for j in range(width):
-            if random.choice([0, 1]) == 0:
-                line += '.'
+            if correct < coin:
+                if random.choice([0, 1]) == 0:
+                    line += '.'
+                else:
+                    line += '#'
+                    correct += 1
             else:
-                line += '#'
+                line += '.'
         ans.append(line)
         line = ''
+        correct = 0
     with open(os.path.join('maps', name), 'w') as map_file:
         for i in range(len(ans) - 1):
             map_file.write(ans[i] + '\n')
