48a6004164240b759803f4401ae7661df120f43d
razgonyaevm <razgonyaevm@gmail.com>
2022-02-13 16:15:31 +0500

    При генерации уровня боковые и нижние стороны будут пустые для корректного перехода через границу

--- scripts/generate_map.py
+++ scripts/generate_map.py
@@ -7,13 +7,15 @@ def generate_map(name, width, height, coin=100000):
     файл автоматически сохраняется в апку maps;
     width - длина каждой строчки
     height - количество строк в файле (не 0)
-    coin - необязательный параметр, нужен для упрощения уровня (ограничивает количество блоков в каждой строке)"""
+    coin - необязательный параметр, нужен для упрощения уровня (ограничивает количество блоков в каждой строке)
+    При этом нижние, верхние и боковые линии должны быть свободны от блоков"""
 
     line = ''
     correct = 0
-    ans = []
-    for i in range(height):
-        for j in range(width):
+    ans = ['.' * width]
+    for i in range(height - 2):
+        line += '.'
+        for j in range(width - 2):
             if correct < coin:
                 if random.choice([0, 1]) == 0:
                     line += '.'
@@ -22,13 +24,16 @@ def generate_map(name, width, height, coin=100000):
                     correct += 1
             else:
                 line += '.'
+        line += '.'
         ans.append(line)
         line = ''
         correct = 0
-    with open(os.path.join('maps', name), 'w') as map_file:
+    ans.append('.' * width)
+    with open(os.path.join('../maps', name), 'w') as map_file:
         for i in range(len(ans) - 1):
             map_file.write(ans[i] + '\n')
         map_file.write(ans[-1])
 
 
-generate_map('random_map.txt', 20, 18)
+if __name__ == '__main__':
+    generate_map('random_map.txt', 20, 18)
