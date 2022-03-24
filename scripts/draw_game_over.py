575350a97d235364fa0f952b37d063650a92ed45
razgonyaevm <razgonyaevm@gmail.com>
2022-03-24 13:53:07 +0500

    commit

---
+++ scripts/draw_game_over.py
@@ -0,0 +1,14 @@
+import pygame.font
+
+
+def draw_game_over(screen, width, height):
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render("GAME OVER!", True, (100, 255, 100))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (255, 0, 0), (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
+å
